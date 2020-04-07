513340b2f651f4e3cc7cba4de1b5ae6257199551
rhn <gihu.rhn@porcupinefactory.org>
2020-04-07 11:11:46 +0000

    Watchdog works

--- src/main.c
+++ src/main.c
@@ -38,6 +38,31 @@ void simpleDelay(unsigned int noOfLoops){
     }
 }
 
+/// Results in a tick every 9765.625 Hz (CPU @ 20MHz, PBCLK @ 1/8)
+void timer_init() {
+	T2CON = 0;
+	T3CON = 0;
+	T2CON = 0x0078;
+    TMR2 = 0;
+    PR2 = 0xffffffff; // max period
+    T2CON = 0x8078; // ON=1 | TCKPS=0b111 (1:256), use PBCLK, T32=timer3
+}
+
+uint32_t timer_read() {
+	return TMR2;
+}
+
+void timer_reset() {
+	timer_init();
+}
+
+// ~20 min in timer ticks
+//const uint32_t timer_trigger = (20 * 60) * 9765.625;
+const uint32_t timer_trigger = 10 * 60 * 9765.625;
+
+// One second to hold
+const uint32_t timer_hold = 9765.625;
+
 void setup(){
 	// What is the equivalent of SYSTEMConfigPerformance?
 	// -> Setting up the system for the required System Clock
@@ -55,12 +80,12 @@ void setup(){
 #if defined (__32MX270F256D__)
 	SystemConfig(48000000L, 1);	// Set to 48MHz, with PBCLK with divider 1 (same settings as DEVCFG)
 #elif defined(__32MX440F256H__)
-	SystemConfig(80000000L, 1);	// Set to 80MHz
+	SystemConfig(20000000L, 8);
 #endif
 
 	LED_init();
 	BTN_init();
-	UARTDrv_Init(115200);
+	timer_init();
 
 	// Enable DMA. This was enabled during testing USB, TODO check.
 	DMACONbits.ON = 1;
@@ -94,9 +119,23 @@ static void send_string_sync(uint8_t endpoint, const char *str)
 }
 */
 
-int main(){
-	bool needs_hello = false;
+const char hex_tab[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+	'a', 'b', 'c', 'd', 'e', 'f'};
 
+void to_string_16b(char buf[4], uint16_t data) {
+	buf[3] = hex_tab[data & 0xf];
+	buf[2] = hex_tab[(data >> 4) & 0xf];
+	buf[1] = hex_tab[(data >> 8) & 0xf];
+	buf[0] = hex_tab[(data >> 12) & 0xf];
+}
+
+void to_string_32b(char buf[8], uint32_t data) {
+	to_string_16b(buf, data >> 16);
+	to_string_16b(buf + 4, data);
+}
+
+int main(){
+	bool needs_info = false;
 	setup();
 	usb_init();
 
@@ -104,19 +143,31 @@ int main(){
 	// Currently everything is hardcoded. Will be expanded later.
 
 	for(;;){
+		uint32_t time = timer_read();
+		if (time > timer_trigger) {
+			LED_setState(0);
+		} else {
+			LED_setState(1);
+		}
+		
+		// Don't hold the reset wire forever, we may want to turn the device back on
+		if (time > timer_trigger + timer_hold) {
+			timer_reset();
+		}
 
 		// Send data to the PC if anything in buffer
 		if (usb_is_configured() && !usb_in_endpoint_halted(2) && !usb_in_endpoint_busy(2)) {
 			uint32_t i;
 			uint8_t *buf = usb_get_in_buffer(2);
-			if (needs_hello) {
-				const char *hello = "hello";
-				strncpy(buf, hello, strlen(hello));
-				usb_send_in_buffer(2, strlen(hello));
-				needs_hello = false;
+			if (needs_info) {
+				const char *pre = "0x";
+				strncpy(buf, pre, strlen(pre));
+				to_string_32b(buf + strlen(pre), time);
+				const char *post = "\r\n";
+				strncpy(buf + strlen(pre) + 8, post, strlen(post));
+				usb_send_in_buffer(2, strlen(pre) + 8 + strlen(post));
+				needs_info = false;
 			}
-			LED_toggle();
-//			LED_setState(enqueued_hello);
 			/*
 			// Send a zero-length packet if the transaction length was the same as the endpoint
 			// length. This is for demo purposes. In real life, you only need to do this if the data
@@ -132,14 +183,14 @@ int main(){
 
 		// Handle data received from the host
 		if (usb_is_configured() && !usb_out_endpoint_halted(2) && usb_out_endpoint_has_data(2)) {
-			needs_hello = true;
+			needs_info = true;
 			const unsigned char *out_buf;
 			size_t out_buf_len;
-			LED_toggle();
 			// Check for an empty transaction.
 			out_buf_len = usb_get_out_buffer(2, &out_buf);
 			if (out_buf_len > 0){
-				// send out echo
+				// TODO: send out echo
+				timer_reset();
 			}
 			usb_arm_out_endpoint(2);
 		}
